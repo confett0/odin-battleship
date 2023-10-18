2e2b878a8b818ebec8268cdc581ba51528cc5e9b
confett0 <118047961+confett0@users.noreply.github.com>
2023-10-18 18:41:25 +0200

    Fix gameboard.board

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,7 +1,15 @@
+import {}
+
 class Gameboard {
     constructor(player) {
         this.player = player;
-        board = Array.from(Array(10), () => new Array(10));
+        board = Array(10).fill().map(() => 
+               Array(10).fill({hasShip: false, isHit: false}));
+    }
+
+    placeShip([x,y],[a,b]) {
+        const length = Math.max((Math.abs(x - a)),(Math.abs(y - b)));
+        
     }
 }
 
