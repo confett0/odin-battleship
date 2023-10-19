dd170b1d059dfd8eb04d504c33c5e7847ce1ae67
confett0 <118047961+confett0@users.noreply.github.com>
2023-10-19 17:37:57 +0200

    Add second gameboard test and create placeShip method

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -8,7 +8,20 @@ export class Gameboard {
     }
 
     placeShip([x,y],[a,b]) {
-        const length = Math.max((Math.abs(x - a)),(Math.abs(y - b))) + 1;
-        const ship = new Ship(length);
+        if (x !== a && y !== b) return;
+
+        const shipLength = Math.max((Math.abs(x - a)),(Math.abs(y - b))) + 1;
+        const ship = new Ship(shipLength);
+
+        if (y === b) {
+            for (let i = x; i <= shipLength; i++) {
+            this.board[i][y].hasShip = true;
+            }
+        }
+        if (x === a) {
+            for (let i = y; i <= shipLength; i++) {
+            this.board[x][i].hasShip = true;
+            }
+        }
     }
 }
--- src/modules/tests/gameboard.test.js
+++ src/modules/tests/gameboard.test.js
@@ -2,10 +2,16 @@ import { Gameboard } from "../gameboard";
 
 describe('Gameboard', () => {
 
-    it('should place ship in the given coordinates', () => {
+    it('should place ship in the given coordinates horizontally', () => {
       const gameboard = new Gameboard();
-      gameboard.placeShip([0,0],[0,3]);
-      expect(gameboard.board[0][2].hasShip).toBe(true);
+      gameboard.placeShip([0,0],[3,0]);
+      expect(gameboard.board[2][0].hasShip).toBe(true);
     });
+
+    it('should place ship in the given coordinates vertically', () => {
+        const gameboard = new Gameboard();
+        gameboard.placeShip([2,2],[2,4]);
+        expect(gameboard.board[2][3].hasShip).toBe(true);
+      });
 
   });
