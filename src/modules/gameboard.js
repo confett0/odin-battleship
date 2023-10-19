dca8a530098895ffa85ddea4c81706c28f3893b5
confett0 <118047961+confett0@users.noreply.github.com>
2023-10-19 17:23:41 +0200

    Fix typo and add first gameboard test

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,14 +1,14 @@
-import Ship from "./ship";
+import { Ship } from "./ship";
 
 export class Gameboard {
     constructor(player) {
         this.player = player;
-        board = Array(10).fill().map(() => 
+        this.board = Array(10).fill().map(() => 
                Array(10).fill({hasShip: false, isHit: false}));
     }
 
     placeShip([x,y],[a,b]) {
-        const length = Math.max((Math.abs(x - a)),(Math.abs(y - b)));
-        
+        const length = Math.max((Math.abs(x - a)),(Math.abs(y - b))) + 1;
+        const ship = new Ship(length);
     }
 }
--- src/modules/tests/gameboard.test.js
+++ src/modules/tests/gameboard.test.js
@@ -1,1 +1,11 @@
 import { Gameboard } from "../gameboard";
+
+describe('Gameboard', () => {
+
+    it('should place ship in the given coordinates', () => {
+      const gameboard = new Gameboard();
+      gameboard.placeShip([0,0],[0,3]);
+      expect(gameboard.board[0][2].hasShip).toBe(true);
+    });
+
+  });
