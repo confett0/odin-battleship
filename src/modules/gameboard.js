c311a842b211d137339fc37c4453d53219940212
confett0 <118047961+confett0@users.noreply.github.com>
2023-10-19 18:14:47 +0200

    Add name property to Ship class

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -4,23 +4,23 @@ export class Gameboard {
     constructor(player) {
         this.player = player;
         this.board = Array(10).fill().map(() =>
-            Array(10).fill({ hasShip: false, isHit: false }));
+            Array(10).fill({ ship: null, isHit: false }));
     }
 
-    placeShip([x, y], [a, b]) {
+    placeShip([x, y], [a, b], shipName) {
         if (x !== a && y !== b) return;
 
         const shipLength = Math.max((Math.abs(x - a)), (Math.abs(y - b))) + 1;
-        const ship = new Ship(shipLength);
+        const ship = new Ship(shipLength, shipName);
 
         if (y === b) {
             for (let i = x; i <= shipLength; i++) {
-                this.board[i][y].hasShip = true;
+                this.board[i][y].ship = ship.name;
             }
         }
         if (x === a) {
             for (let i = y; i <= shipLength; i++) {
-                this.board[x][i].hasShip = true;
+                this.board[x][i].ship = ship.name;
             }
         }
     }
--- src/modules/tests/gameboard.test.js
+++ src/modules/tests/gameboard.test.js
@@ -4,14 +4,14 @@ describe('Gameboard', () => {
 
     it('should place ship in the given coordinates horizontally', () => {
       const gameboard = new Gameboard();
-      gameboard.placeShip([0,0],[3,0]);
-      expect(gameboard.board[2][0].hasShip).toBe(true);
+      gameboard.placeShip([0,0],[3,0], "Battleship");
+      expect(gameboard.board[2][0].ship).toBe("Battleship");
     });
 
     it('should place ship in the given coordinates vertically', () => {
         const gameboard = new Gameboard();
-        gameboard.placeShip([2,2],[2,4]);
-        expect(gameboard.board[2][3].hasShip).toBe(true);
+        gameboard.placeShip([2,2],[2,4], "Destroyer");
+        expect(gameboard.board[2][3].ship).toBe("Destroyer");
       });
     
       it('should records the coordinates of a missed shot', () => {
