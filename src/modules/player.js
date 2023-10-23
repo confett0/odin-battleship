31b75fb146e980b0196dbb77428857a3887b8618
confett0 <118047961+confett0@users.noreply.github.com>
2023-10-23 17:27:04 +0200

    Create Computer class and attack method

--- src/modules/player.js
+++ src/modules/player.js
@@ -5,3 +5,20 @@ export class Player {
 
     // turn ?
 }
+
+export class Computer extends Player {
+    constructor() {
+        this.moves = [];
+    }
+    attack() {
+        const getRandomNumber = () => Math.floor(Math.random * 10);
+        const a = getRandomNumber();
+        const b = getRandomNumber();
+        if (!this.moves.includes([a, b])) {
+            this.moves.push([a,b]);
+            return [a, b]
+        } else {
+            this.attack();
+        }
+    }
+}
