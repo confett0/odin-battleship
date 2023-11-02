6048267e5cfcc51a0a65164558bcbc09c9e01fae
confett0 <118047961+confett0@users.noreply.github.com>
2023-11-02 15:54:33 +0100

    Add delay to computer move

--- src/modules/game.js
+++ src/modules/game.js
@@ -2,27 +2,28 @@ import { Player, Computer } from "./player";
 import { displayShips, updateBoards } from "./ui";
 
 export const game = {
-    
-    player: new Player("player"),
-    computer: new Computer("computer"),
+  player: new Player("player"),
+  computer: new Computer("computer"),
 
-    init() {
-        this.player.gameboard.autoPopulateBoard();
-        this.computer.gameboard.autoPopulateBoard();
-        displayShips(this.player.gameboard);
-    },
+  init() {
+    this.player.gameboard.autoPopulateBoard();
+    this.computer.gameboard.autoPopulateBoard();
+    displayShips(this.player.gameboard);
+  },
 
-    playRound(coord) {
-       this.computer.gameboard.receiveAttack(coord);
-       updateBoards(this.computer);
-        // const computerMove = this.computer.attack();
-       // setTimeout(this.player.gameboard.receiveAttack(computerMove), 1000);
-    }
+  playRound(coord) {
+    this.computer.gameboard.receiveAttack(coord);
+    updateBoards(this.computer);
+    const computerMove = this.computer.attack();
+    setTimeout(() => {
+      game.player.gameboard.receiveAttack(computerMove);
+      updateBoards(game.player);
+    }, 1000);
+  },
 
-    /* checkWinner() {
+  /* checkWinner() {
         if (this.computer.gameboard.allSunk() || this.player.gameboard.allSunk()) {
             // end game
         }
     }*/
-
-}
+};
