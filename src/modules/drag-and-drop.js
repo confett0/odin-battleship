1b52b56922d08176936b850953dbc16e27e4957c
confett0 <118047961+confett0@users.noreply.github.com>
2023-11-08 16:22:23 +0100

    Edit drag and drop to append draggable ship

--- src/modules/drag-and-drop.js
+++ src/modules/drag-and-drop.js
@@ -1,55 +1,65 @@
 export const dragAndDrop = () => {
-    /* draggable element */
-const item = document.querySelector('.drag-ship');
+    // Draggable container for the ship
+    const shipContainer = document.querySelector('.drag-ship');
 
-item.addEventListener('dragstart', dragStart);
+    shipContainer.addEventListener('dragstart', dragStart);
 
-function dragStart(e) {
-    console.log(e);
-    e.dataTransfer.setData('text/plain', e.target.id);
-    setTimeout(() => {
-        e.target.classList.add('hidden');
-    }, 0);
-}
+    function dragStart(e) {
+        e.dataTransfer.setData('text/plain', e.target.id);
+        setTimeout(() => {
+            e.target.classList.add('hidden');
+        }, 0);
+    }
 
+    // Drop targets
+    const cells = document.querySelectorAll('.cell');
 
-/* drop targets */
-const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('dragenter', dragEnter);
+        cell.addEventListener('dragover', dragOver);
+        cell.addEventListener('dragleave', dragLeave);
+        cell.addEventListener('drop', drop);
+    });
 
-cells.forEach(cell => {
-    cell.addEventListener('dragenter', dragEnter)
-    cell.addEventListener('dragover', dragOver);
-    cell.addEventListener('dragleave', dragLeave);
-    cell.addEventListener('drop', drop);
-});
+    function dragEnter(e) {
+        e.preventDefault();
+        e.target.classList.add('drag-over');
+    }
 
+    function dragOver(e) {
+        e.preventDefault();
+        e.target.classList.add('drag-over');
+    }
 
-function dragEnter(e) {
-    e.preventDefault();
-    e.target.classList.add('drag-over');
-}
+    function dragLeave(e) {
+        e.target.classList.remove('drag-over');
+    }
 
-function dragOver(e) {
-    e.preventDefault();
-    e.target.classList.add('drag-over');
-}
+    function drop(e) {
+        e.preventDefault();
+        console.log(e.target);
+        e.target.classList.remove('drag-over');
 
-function dragLeave(e) {
-    e.target.classList.remove('drag-over');
-}
+        const id = e.dataTransfer.getData('text/plain');
+        const draggable = document.getElementById(id);
 
-function drop(e) {
-    e.target.classList.remove('drag-over');
-    console.log(e.dataTransfer);
+        e.target.appendChild(draggable);
 
-    // get the draggable element
-    const id = e.dataTransfer.getData('text/plain');
-    const draggable = document.getElementById(id);
+        /* if (draggable.classList.contains('drag-ship')) {
+            // Check if the target cell and the 4 following cells are empty
+            const targetCellIndex = Array.from(cells).indexOf(e.target);
+            const cellsToFill = Array.from(cells).slice(targetCellIndex, targetCellIndex + 5);
 
-    // add it to the drop target
-    e.target.classList.add("has-ship");
+            if (cellsToFill.every(cell => !cell.classList.contains('has-ship'))) {
+                // Add the entire ship to the 5 consecutive cells
+                cellsToFill.forEach(cell => {
+                    // cell.classList.add("has-ship");
+                    cell.appendChild(draggable.cloneNode(true));
+                });
 
-    // display the draggable element
-    draggable.classList.remove('hidden');
-}
+                // Display the draggable element
+                draggable.classList.remove('hidden');
+            }
+        }*/
+    } 
 }
