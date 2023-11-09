e120f8b1150ef699b40c204485c1806234592f5e
confett0 <118047961+confett0@users.noreply.github.com>
2023-11-09 19:27:51 +0100

    Add rotate ship functionality

--- src/modules/drag-and-drop.js
+++ src/modules/drag-and-drop.js
@@ -1,55 +1,69 @@
 export const dragAndDrop = () => {
-    // Draggable container for the ship
-    const ships = document.querySelectorAll('.drag-ship');
+  
+    const changeAxisButton = document.querySelector(".set-axis");
+    changeAxisButton.addEventListener("click", () => {
+        changeAxisButton.innerText === "Axis: horizontal" ? changeAxisButton.innerText = "Axis: vertical" : changeAxisButton.innerText = "Axis: horizontal";
+    })
+  const ships = document.querySelectorAll(".drag-ship");
+  ships.forEach((ship) => ship.addEventListener("dragstart", dragStart));
 
-    ships.forEach(ship => ship.addEventListener('dragstart', dragStart));
+  function dragStart(e) {
+    e.dataTransfer.setData("text/plain", e.target.id);
+    if (changeAxisButton.innerText === "Axis: vertical") {
+    e.target.classList.add("vertical");
+}
+    setTimeout(() => {
+      e.target.classList.add("hidden");
+    }, 0);
+  }
 
-    function dragStart(e) {
-        e.dataTransfer.setData('text/plain', e.target.id);
-        setTimeout(() => {
-            e.target.classList.add('hidden');
-        }, 0);
-    }
+  // Drop targets
+  const cells = document.querySelectorAll(".cell");
 
-    // Drop targets
-    const cells = document.querySelectorAll('.cell');
+  cells.forEach((cell) => {
+    cell.addEventListener("dragenter", dragEnter);
+    cell.addEventListener("dragover", dragOver);
+    cell.addEventListener("dragleave", dragLeave);
+    cell.addEventListener("drop", drop);
+  });
 
-    cells.forEach(cell => {
-        cell.addEventListener('dragenter', dragEnter);
-        cell.addEventListener('dragover', dragOver);
-        cell.addEventListener('dragleave', dragLeave);
-        cell.addEventListener('drop', drop);
-    });
+  function dragEnter(e) {
+    e.preventDefault();
+    e.target.classList.add("drag-over");
+  }
 
-    function dragEnter(e) {
-        e.preventDefault();
-        e.target.classList.add('drag-over');
-    }
+  function dragOver(e) {
+    e.preventDefault();
+    e.target.classList.add("drag-over");
+  }
 
-    function dragOver(e) {
-        e.preventDefault();
-        e.target.classList.add('drag-over');
-    }
+  function dragLeave(e) {
+    e.target.classList.remove("drag-over");
+  }
 
-    function dragLeave(e) {
-        e.target.classList.remove('drag-over');
-    }
+  function drop(e) {
+    e.preventDefault();
+    e.target.classList.remove("drag-over");
 
-    function drop(e) {
-        e.preventDefault();
-        e.target.classList.remove('drag-over');
+    const id = e.dataTransfer.getData("text/plain");
+    const draggableShip = document.getElementById(id);
+    const shipLength = draggableShip.getAttribute("data-length");
+    const gridLength = 10;
 
-        const id = e.dataTransfer.getData('text/plain');
-        const draggableShip = document.getElementById(id);
-        const shipLength = draggableShip.getAttribute('data-length');
-        const gridLength = 10;
-
+    if (draggableShip.classList.contains("vertical")) {
+        if (e.target.dataset.coordY <= gridLength - shipLength) {
+            e.target.appendChild(draggableShip);
+            draggableShip.classList.add("positioned-ship");
+          }
+    } else {
         if (e.target.dataset.coordX <= gridLength - shipLength) {
-        e.target.appendChild(draggableShip);
-        draggableShip.classList.add("positioned-ship");
-        }
+            e.target.appendChild(draggableShip);
+            draggableShip.classList.add("positioned-ship");
+          }
+    }
 
-        /* if (draggable.classList.contains('drag-ship')) {
+
+    /* if (draggable.classList.contains('drag-ship')) {
             // Check if the target cell and the 4 following cells are empty
             const targetCellIndex = Array.from(cells).indexOf(e.target);
             const cellsToFill = Array.from(cells).slice(targetCellIndex, targetCellIndex + 5);
@@ -65,5 +79,5 @@ export const dragAndDrop = () => {
                 draggable.classList.remove('hidden');
             }
         }*/
-    } 
-}
+  }
+};
